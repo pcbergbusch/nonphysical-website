8d90342b3572a7612bdcd22ce4584554001815a9
pbergbusch <bergbusch.wp@triton-partners.com>
2025-06-24 11:27:18 +0200

    add .nojekyll

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ command, mode }) => ({
+  base: command === 'build' ? '/nonphysical-website/' : '/',
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
